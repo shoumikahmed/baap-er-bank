84b531ea5fd19127e73afa3ab3038dae38573c75
shoumikahmed <shoumikahmed103@gmail.com>
2022-02-11 09:59:23 +0600

    baaperbank

---
+++ js/login.js
@@ -0,0 +1,15 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    // get user email
+    const emailField = document.getElementById('user-email')
+    const userEmail = emailField.value;
+    console.log(userEmail);
+    // get user password
+    const passwordField = document.getElementById('user-password')
+    const userPassword = passwordField.value;
+    // check email and password
+    if (userEmail == 'sontan@baap.com' && userPassword == 'secret') {
+        window.location.href = 'banking.html'
+    }
+})
+
+
